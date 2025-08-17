54027bbbce9c1b0687eff00d9fd86ce150e97444
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-08-17 07:34:55 +0530

    done

--- linkedlist_class/com/krush/linked_list/CustomSinglyLinkList.java
+++ linkedlist_class/com/krush/linked_list/CustomSinglyLinkList.java
@@ -1,38 +1,33 @@
 package com.krush.linked_list;
 
-import java.util.LinkedList;
+//import java.util.LinkedList;
 
-public class CustomSinglyLinkList
-{
+public class CustomSinglyLinkList {
 
-	
-	private static class Node
-	{
+	private static class Node {
 		int item;
 		Node next;
-		
+
 		public Node(int item) {
-			this.item=item;
-			next=null;
+			this.item = item;
+			next = null;
 		}
-		
-		
-		
-		
+
 	}
+
 	public static void main(String[] args) {
-	   Node n1 = new Node(100);
-	   Node n2 = new Node(200);
-	   Node n3 = new Node(300);
-	      // System.out.println(n1.next.next);  NullPointerException
-	   
-	    //Singly linked list
-	      n1.next =n2;
-	      n2.next= n3;
-	    System.out.println(n1.item);
-	    System.out.println(n1.next.item);
-	    System.out.println(n1.next.next.item);
-	    
+		Node n1 = new Node(100);
+		Node n2 = new Node(200);
+		Node n3 = new Node(300);
+		// System.out.println(n1.next.next); NullPointerException
+
+		// Singly linked list
+		n1.next = n2;
+		n2.next = n3;
+		System.out.println(n1.item);
+		System.out.println(n1.next.item);
+		System.out.println(n1.next.next.item);
+
 // note: ==>      LinkedList l= new LinkedList();  //doubly linked list implementation of our LikedList Class 
 	}
 
